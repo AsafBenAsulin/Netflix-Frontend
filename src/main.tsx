f498f7ea65a5eca7a29c031a73c8147701d7c68f
AsafBenAsulin <156786762+AsafBenAsulin@users.noreply.github.com>
2024-03-07 13:29:30 +0200

    bug fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from './App.tsx'
 import axios from 'axios'
 
 
-const baseURL = import.meta.env.VITE_BASE_URL || 'http://localhost:3000';
+const baseURL = import.meta.env.VITE_BASE_URL;
 axios.defaults.baseURL = baseURL;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
